123bd30c6b36ae0f43f8e8e86a00f9a7e6315ce4
eleven <123456@qq.com>
2018-11-03 19:47:57 +0800

    20181103-office

--- app/Http/Controllers/backend/IndexController.php
+++ app/Http/Controllers/backend/IndexController.php
@@ -26,9 +26,8 @@ class IndexController extends MyController
         }
         $date = date('Y-m-d',time());
         //获取当前代理今天到目前为目的佣金总额
-        echo $todayStart = date('Y-m-d',time()).' 00:00:00';
-        echo "<br>";
-        echo $todayEnd = date('Y-m-d',time()).' 23:59:59';
+        $todayStart = date('Y-m-d',time()).' 00:00:00';
+        $todayEnd = date('Y-m-d',time()).' 23:59:59';
         $todaycommision = 0;
         $todaycommisionArray = OrderDeposit::where('daili_id',session('daili_id'))->where('status',1)->where('pay_daili',1)->where('deal_time','>',$todayStart)->where('deal_time','<',$todayEnd)->get()->toArray();
         if(!empty($todaycommisionArray))
